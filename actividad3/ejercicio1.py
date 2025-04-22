587837401f5d07fa50e439383eb86bfe5d56b8f5
AlexMunoz9 <alex.2munoz9.@gmail.com>
2025-04-22 07:32:06 -0600

    calculo de promedio

---
+++ actividad3/ejercicio1.py
@@ -0,0 +1,15 @@
+
+calificacion1 = int(input("Ingrese la calificacion 1: "))
+calificacion2 = int(input("Ingrese la calificacion 2: "))
+calificacion3 = int(input("Ingrese la calificacion 3: "))
+suma = calificacion1 + calificacion2 + calificacion3
+promedio = suma / 3
+
+print(f"""Calificacion 1: {calificacion1}
+calificacion 2: {calificacion2}
+calificacion 3: {calificacion3}
+promedio    {promedio:.0f}""")
+
+
+
+
