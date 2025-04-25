c6a22da1ee0f52bea79ccefb7d873b56b636f716
AlexMunoz9 <alex.2munoz9.@gmail.com>
2025-04-25 07:42:57 -0600

    Calificacion

--- act/ejercicio2.py
+++ act/ejercicio2.py
@@ -1,1 +1,10 @@
 sueldo = float(input("Introduce el sueldo: "))
+bono = 0
+if sueldo > 3000:
+    print("El bono sera el 10% del sueldo")
+    bono = sueldo * 0.10
+elif sueldo >= 1500 and sueldo <= 3000:
+    print("El bono sera el 5% del sueldo")
+    bono = sueldo * 0.05
+elif sueldo <= 1500:
+    print("No hay bono")
